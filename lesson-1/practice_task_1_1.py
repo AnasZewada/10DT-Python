ef719d92c8db6e1c834e8a40fe78d457e4d1a63f
AnasZewada <100626059+AnasZewada@users.noreply.github.com>
2022-08-31 10:36:27 +1200

    Add files via upload

---
+++ lesson-1/practice_task_1_1.py
@@ -0,0 +1,31 @@
+# Anas Zewada
+
+#Practice task-variables, data types, print, comments
+
+x = 32
+
+print(x)
+
+name = "Anas Zewada"
+height = 190
+adress = "newlands"
+phone = '049391234'
+hasAllergy = False
+
+#summary of info
+print(name)
+print(height)
+print(adress)
+print(phone)
+print(hasAllergy)
+
+#clarifying the problem
+#height is in cm
+#adress is stored as ONLY suburb
+#country is not required
+#no allergies
+# data to be displayed in conetext of readable language
+
+
+
+
