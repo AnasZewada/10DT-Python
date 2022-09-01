e0c8c090a7d6acae1da877214d465ff66ad2616f
AnasZewada <100626059+AnasZewada@users.noreply.github.com>
2022-09-01 12:13:56 +1200

    Add files via upload

---
+++ lesson-1/input_user.py
@@ -0,0 +1,32 @@
+# Anas Zewada
+
+#input- variation types by user
+
+# input() function
+
+#taking input from the user
+#string = input()
+#output
+#print()
+
+#name = input("Enter Your Name: ")
+#print("Hello," ,name)
+
+# Taking input from the user as a interger
+#num = int(input("Enter a number: "))
+#add = num + 1
+#output
+#print(add)
+
+#Taking input from the user as a float
+#num = float(input("Enter a number: "))
+#add = num + 1
+#output
+#print(add)
+
+#Taking input from the user as a list
+#li = list(input("Enter a number: "))
+#output
+#print(li)
+
+
