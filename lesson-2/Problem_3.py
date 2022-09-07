4d4389c37b064e445c37cdd9c6591ba53eeedd17
AnasZewada <100626059+AnasZewada@users.noreply.github.com>
2022-09-08 11:59:07 +1200

    Add files via upload

---
+++ lesson-2/Problem_3.py
@@ -0,0 +1,20 @@
+# Anas Zewada
+# concentration in output, '+' as a seperator, introducing a constant variable value
+
+
+
+dieselPrice = 1.47
+# remember varieble names cannot begin with a digit
+petrol95Price = 2.17
+petrol91Price = 2.86
+print("Initial price:")
+print("Diesel price $" + str(dieselPrice))
+print("95 price $" + str (petrol95Price))
+print("91 unleaded price $" + str(petrol91Price))
+dieselPrice = dieselPrice * 1.1
+petrol95Price = petrol95Price * 1.1
+petrol91Price = petrol91Price * 1.1
+print("Price Increase of 10%:")
+print("Diesel price $" + str(dieselPrice))
+print("95 price $" + str (petrol95Price))
+print("91 unleaded price $" + str(petrol91Price))
