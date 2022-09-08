a72bf42a08441bfe7d0bc8a2234e4b2826cf93a2
AnasZewada <100626059+AnasZewada@users.noreply.github.com>
2022-09-08 12:24:05 +1200

    Add files via upload

---
+++ lesson-2/variable _print_with_dollar_sign.py
@@ -0,0 +1,16 @@
+# Anas Zewada
+# end for concatenation in output
+
+
+dollars = 5
+print("$", dollars, ".00")
+
+dollars = 5
+print("$", end = '')
+print(dollars, end='')
+print(".00")
+
+winner = "water boy"
+print("First place", winner) #note the blank space between ''
+print("First place", end= '') #note the blank space between ''
+print(winner)
